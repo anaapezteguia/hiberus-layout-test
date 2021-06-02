7c6e1ba6a03d6a860bd45ebbc41e522e3bb25a92
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-06-02 10:46:05 +0200

    Add hero images and some JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('>> Ready :)');
+let imageBig = document.getElementById('heroBig');
+let imageSmall = document.getElementById('heroSmall');
+let currentPos = 0;
+const imagesBig = [
+  'virentia1.jpg',
+  'virentia2.jpg',
+  'virentia3.png',
+  'virentia4.jpg',
+];
+const imagesSmall = [
+  'virentia1_450.jpg',
+  'virentia2_450.jpg',
+  'virentia3_450.png',
+  'virentia4_450.jpg',
+];
+
+function changePic() {
+  if (++currentPos >= imagesBig.length || ++currentPos >= imagesSmall.length)
+    currentPos = 0;
+  imageSmall.src = './assets/images/' + imagesSmall[currentPos];
+  imageBig.src = './assets/images/' + imagesBig[currentPos];
+}
+
+setInterval(changePic, 3000);
